2ac718755324bb10c3604c90633c02b39a0ba82c
Divine Enyinnayah <enyinnayahd@gmail.com>
2025-06-17 10:55:56 +0100

    The sign up page and other pages are working

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+"use client"; // required for event handlers
+
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href="/">Home</Link> | <Link href="/about">About</Link> | <Link href="/contact">Contact</Link>
+    </nav>
+  );
+}
